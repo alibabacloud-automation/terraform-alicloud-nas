32c012e77ed3932ac9a0b6c27c84eca9775b248d
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2021-12-15 15:03:21 +0800

    Improves the module examples/complete

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,61 +1,69 @@
-module "module_vpc" {
-  source        = "alibaba/vpc/alicloud"
-  vpc_name      = "my_module_vpc"
-  vswitch_name  = "my_module_vswitch"
-  vswitch_cidrs = ["172.16.1.0/24"]
-}
-
 module "file_system" {
-  source = "../modules/nas_file_system"
+  source = "../../"
 
   ###############################################################
   #variables for file system
   ##############################################################
-  protocol_type = var.file_system_protocol_type
+  file_system_protocol_type = var.file_system_protocol_type
 
-  create_file_system      = var.create_file_system
-  storage_type            = var.file_system_storage_type
-  file_system_description = var.file_system_description
+  create_file_system       = true
+  file_system_storage_type = var.file_system_storage_type
+  file_system_description  = var.file_system_description
 }
 
 module "access_group" {
-  source = "../modules/nas_access_group"
+  source = "../../"
 
   ###############################################################
   #variables for access group
   ##############################################################
   access_group_name = var.access_group_name
 
-  create_access_group      = var.create_access_group
+  create_access_group      = true
   access_group_type        = var.access_group_type
   access_group_description = var.access_group_description
 }
 
 module "access_rule" {
-  source = "../modules/nas_access_rule"
+  source = "../../"
 
   ###############################################################
   #variables for access rule
   ##############################################################
   access_group_name = module.access_group.this_access_group_name
 
-  create_access_rule   = var.create_access_rule
+  create_access_rule   = true
   source_cidr_ip       = var.source_cidr_ip
   rw_access_type       = var.rw_access_type
   user_access_type     = var.user_access_type
   access_rule_priority = var.access_rule_priority
 }
 
+data "alicloud_zones" "this" {
+  available_resource_creation = "VSwitch"
+}
+
+module "vpc" {
+  source             = "alibaba/vpc/alicloud"
+  create             = true
+  vpc_name           = "my_module_vpc"
+  vpc_cidr           = "172.16.0.0/16"
+  vswitch_name       = "my_module_vswitch"
+  vswitch_cidrs      = ["172.16.1.0/24"]
+  availability_zones = [data.alicloud_zones.this.ids.0]
+}
+
+
 module "mount_target" {
-  source = "../modules/nas_mount_target"
+  source = "../../"
 
   ###############################################################
   #variables for mount target
   ##############################################################
   access_group_name = module.access_group.this_access_group_name
 
-  create_mount_target = var.create_mount_target
-  file_system_id      = var.create_file_system ? module.file_system.this_file_system_id : var.file_system_id
-  vswitch_id          = module.module_vpc.vswitch_ids
+  create_mount_target = true
+  file_system_id      = module.file_system.this_file_system_id
+  vswitch_id          = module.vpc.vswitch_ids[0]
 }
 
--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "region" {
   description = "(Deprecated from version 1.2.0) The region used to launch this module resources."
+  type        = string
   default     = ""
 }
 
@@ -8,21 +9,25 @@ variable "region" {
 ##############################################################
 variable "access_group_description" {
   description = "The description of nas access group."
+  type        = string
   default     = ""
 }
 
 variable "access_group_type" {
   description = "The type of nas access group."
+  type        = string
   default     = "Vpc"
 }
 
 variable "access_group_name" {
   description = "The name of nas access group."
+  type        = string
   default     = ""
 }
 
 variable "create_access_group" {
   description = "Determine whether a permission group exists of access group."
+  type        = bool
   default     = false
 }
 
@@ -31,21 +36,25 @@ variable "create_access_group" {
 ##############################################################
 variable "file_system_protocol_type" {
   description = "The protocol_type of file system."
+  type        = string
   default     = "NFS"
 }
 
 variable "create_file_system" {
   description = "Judging whether file system exist of file sytem."
+  type        = bool
   default     = false
 }
 
 variable "file_system_storage_type" {
   description = "The storage_type of file system."
+  type        = string
   default     = "Performance"
 }
 
 variable "file_system_description" {
   description = "The description of nas file system."
+  type        = string
   default     = ""
 }
 
@@ -54,26 +63,31 @@ variable "file_system_description" {
 ##############################################################
 variable "source_cidr_ip" {
   description = "The source_cidr_ip of an existing access rule."
+  type        = string
   default     = ""
 }
 
 variable "rw_access_type" {
   description = "The rw_access_type of access rule."
+  type        = string
   default     = "RDWR"
 }
 
 variable "user_access_type" {
   description = "The user_access_type of access rule."
+  type        = string
   default     = "no_squash"
 }
 
 variable "access_rule_priority" {
   description = "The priority of access rule."
+  type        = number
   default     = 1
 }
 
 variable "create_access_rule" {
   description = "Judging whether permission rules exist of access rule."
+  type        = bool
   default     = false
 }
 
@@ -82,16 +96,19 @@ variable "create_access_rule" {
 ##############################################################
 variable "create_mount_target" {
   description = "Judging whether mount target exist of mount target."
+  type        = bool
   default     = false
 }
 
 variable "file_system_id" {
   description = "The mount_target_domain of nas mount target."
+  type        = string
   default     = ""
 }
 
 variable "vswitch_id" {
   description = "The vswitch ids. The vswitch_id fo the mount target."
+  type        = string
   default     = ""
 }
 
